c5554eac2194caf612d8cca978e923856106640a
Diego Lara <diegoalara2005@gmail.com>
2023-12-08 15:01:19 -0600

    Donde esta walter

---
+++ src/lab9p2_carmenbrandonalejandro/HiloEliminarRegistro.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab9p2_carmenbrandonalejandro;
+
+import javax.swing.JProgressBar;
+
+/**
+ *
+ * @author diego
+ */
+public class HiloEliminarRegistro extends Thread{
+     private JProgressBar progBar;
+    private boolean isAlive;
+
+    public HiloEliminarRegistro(JProgressBar progBar, boolean isAlive) {
+        this.progBar = progBar;
+        this.isAlive = isAlive;
+    }
+
+    public JProgressBar getProgBar() {
+        return progBar;
+    }
+
+    public void setProgBar(JProgressBar progBar) {
+        this.progBar = progBar;
+    }
+
+    public boolean isIsAlive() {
+        return isAlive;
+    }
+
+    public void setIsAlive(boolean isAlive) {
+        this.isAlive = isAlive;
+    }
+
+    @Override
+    public void run() {
+
+        while (isAlive) {
+            progBar.setValue(progBar.getValue() + 1);
+            if (progBar.getValue() == 100000000) {
+                isAlive = false;
+            }
+            //FIN IF
+
+            try {
+                Thread.sleep(0);
+            } catch (InterruptedException ex) {
+                
+            }
+        }
+    }
+}
