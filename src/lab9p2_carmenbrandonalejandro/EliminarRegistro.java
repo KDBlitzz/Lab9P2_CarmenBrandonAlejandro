2fb7034ce3b346067cac4ea1bea464fb31920ff5
Diego Lara <diegoalara2005@gmail.com>
2023-12-08 17:31:43 -0600

    Nuevas clases para que no se bugee

---
+++ src/lab9p2_carmenbrandonalejandro/EliminarRegistro.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab9p2_carmenbrandonalejandro;
+
+import java.awt.Color;
+import javax.swing.JProgressBar;
+
+/**
+ *
+ * @author diego
+ */
+public class EliminarRegistro extends Thread{
+    private JProgressBar progBar;
+    private boolean isAlive = true;
+    private boolean avanzar = true;
+
+    public EliminarRegistro(JProgressBar progBar, boolean isAlive, boolean avanzar) {
+        this.progBar = progBar;
+        this.isAlive = isAlive;
+        this.avanzar = avanzar;
+    }
+
+    public JProgressBar getProgBar() {
+        return progBar;
+    }
+
+    public void setProgBar(JProgressBar progBar) {
+        this.progBar = progBar;
+    }
+
+    public boolean isIsAlive() {
+        return isAlive;
+    }
+
+    public void setIsAlive(boolean isAlive) {
+        this.isAlive = isAlive;
+    }
+    @Override
+    public void run(){
+        
+        while(isAlive){
+            if(avanzar){
+                progBar.setForeground(Color.magenta);
+                progBar.setMaximum(10);
+                progBar.setValue(progBar.getValue()+1);
+                if(progBar.getValue() == 10){
+                    isAlive=false;
+                }                
+            } //FIN IF
+            
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException ex) {
+            }
+        }
+    }
+}
